4875f56f442662581068504ed1c6840127c57876
infoocean <infoocean8454@gmail.com>
2022-11-03 19:49:55 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import logo from "./logo.svg";
 import "./App.css";
-
+import Login from "./Mycomponents/Login";
+import Register from "./Mycomponents/Register";
+import { Routes, Route } from "react-router-dom";
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/loginpage" element={<Login />} />
+        <Route path="/registrationpage" element={<Register />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
